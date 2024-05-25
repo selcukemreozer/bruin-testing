dd5805f15d86c6fe1e398bbc9dbae73c1e30934a
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-05-25 23:51:14 +0300

    first commit
    I test bruin tool

---
+++ assets/bruin-test.sql
@@ -0,0 +1,5 @@
+-- @bruin.name: dataset.bruin_test
+-- @bruin.type: bq.sql
+-- @bruin.materialization.type: table
+
+SELECT 1 as result
---
+++ assets/hello.py
@@ -0,0 +1,4 @@
+# @bruin.name: Hello
+# @bruin.depends: dataset.bruin-test
+
+print("Hello, world!")
